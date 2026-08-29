7e1748dd4bd38971cd9f995dc31f33a292f64476
rsl <a73rasoul@gmail.com>
2026-08-30 02:50:48 +0330

    blog: feat: create detail view

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -8,5 +8,6 @@ app_name = "blog"
 urlpatterns = [    
     path('cbv-index', views.IndexView.as_view(),name = 'cbv-index'),
     path('post/', views.PostList.as_view(),name='post-list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(),name='post-detail'),
     path('go-to-maktabkhooneh/<int:pk>/', RedirectToMaktab.as_view(pattern_name="blog:cbv-index"), name = 'redirect-to-index')
 ]
